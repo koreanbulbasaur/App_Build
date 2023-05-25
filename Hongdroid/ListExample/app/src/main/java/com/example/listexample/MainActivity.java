443f5b1ec460bd77b0c48707a8ca94764843e4ab
koreanbulbasaur <koreanbulbasaur99@naver.com>
2023-05-25 11:04:10 +0900

    list

--- Hongdroid/ListExample/app/src/main/java/com/example/listexample/MainActivity.java
+++ Hongdroid/ListExample/app/src/main/java/com/example/listexample/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.listexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView list_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list_test = (ListView)findViewById(R.id.list_test);
+
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list_test.setAdapter(adapter);
+
+        data.add("버스");
+        data.add("전철");
+        data.add("자차");
+        adapter.notifyDataSetChanged();
     }
 }
